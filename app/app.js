155d7153ba238c60664d38dc9abc625075cdf2ff
Anton Baksheiev <abaksheiev@gmail.com>
2015-08-28 21:19:20 +0200

    Move information in separated template

--- app/app.js
+++ app/app.js
@@ -2,3 +2,14 @@
  * Created by Anton on 26.08.2015.
  */
 var myApp = angular.module('app', []);
+
+myApp.run(function($templateCache) {
+    $templateCache.put('info.html',
+        '<small><b>Author:</b> <i>Baksheiev Anton+</i></small>' +
+        '</br>' +
+        '<small><b>linkedin:</b> ' +
+        '<i><a href="https://www.linkedin.com/pub/baksheiev-anton/20/a56/b53">https://www.linkedin.com/pub/baksheiev-anton/20/a56/b53</a></i>'+
+        '</small>' +
+        '</br>'
+    );
+});
